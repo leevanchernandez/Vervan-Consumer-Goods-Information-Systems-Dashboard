442656d7ff335ea9b05994a16be9c12926593925
Leevan Nicoli Choi Hernandez <leevanc.hernandez@gmail.com>
2025-11-23 18:07:12 +0800

    updated dbconnect.py

--- apps/dbconnect.py
+++ apps/dbconnect.py
@@ -12,18 +12,33 @@ def getdblocation():
     )
     
     return db
-def modifyDB(sql, values):
+def modifyDB(sql, values, return_id=False):
     db = getdblocation()
     cursor = db.cursor()
     cursor.execute(sql, values)
+    
+    if return_id:
+        last_id = cursor.fetchone()[0]  # get RETURNING id
+        db.commit()
+        db.close()
+        return last_id
+
     db.commit()
     db.close()
-    
+
+
 def getDataFromDB(sql, values, dfcolumns):
+    # ARGUMENTS
+    # sql -- sql query with placeholders (%s)
+    # values -- values for the placeholders (list or tuple)
+    # dfcolumns -- column names for the output
+
     db = getdblocation()
-    cursor = db.cursor()
-    cursor.execute(sql, values)
-    rows = pd.DataFrame(cursor.fetchall(), columns=dfcolumns)
+    cur = db.cursor()
+    cur.execute(sql, values)
+    rows = pd.DataFrame(cur.fetchall(), columns=dfcolumns)
     db.close()
     return rows
 
+def hash_string(string):
+    return hashlib.sha256(string.encode('utf-8')).hexdigest()
