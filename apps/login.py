0a0750793ca318328ff70db6ad1d35430ac9fee0
Leevan Nicoli Choi Hernandez <leevanc.hernandez@gmail.com>
2025-11-23 16:16:43 +0800

    added login functionality

--- apps/login.py
+++ apps/login.py
@@ -1,10 +1,12 @@
 import dash
 import dash_bootstrap_components as dbc
 from dash import html, dcc
+from dash.dependencies import Input, Output, State
 from dash.exceptions import PreventUpdate
 
 from app import app
 from apps.commonmodules import makeNavbar
+from apps.dbconnect import getDataFromDB
 
 # Coloring
 bg_color = "#1e0f00",
@@ -20,6 +22,7 @@ layout = html.Div(
         # Main Container
         dbc.Container(
             [
+                dcc.Location(id='url_login', refresh=True),
                 html.H1(
                     "Good to see you again!",
                     style={
@@ -34,6 +37,7 @@ layout = html.Div(
                 dbc.Card(
                     dbc.CardBody(
                         [
+                            dbc.Alert(id='login_alert', is_open=False, color="danger"),
                             dbc.Label(
                                 "Enter your username:",
                                 style={
@@ -42,6 +46,7 @@ layout = html.Div(
                                 },
                             ),
                             dbc.Input(
+                                id="login_username",
                                 type="text",
                                 placeholder="ex. vervan1223",
                                 style={
@@ -61,6 +66,7 @@ layout = html.Div(
                                 },
                             ),
                             dbc.Input(
+                                id="login_password",
                                 type="password",
                                 placeholder="ex. 12345678",
                                 style={
@@ -73,7 +79,7 @@ layout = html.Div(
                             ),
                             dbc.Button(
                                 "Log In",
-                                href = "/ownerdashboard",
+                                # href = "/ownerdashboard",
                                 color="secondary",
                                 id="login-button",
                                 n_clicks = 0,
@@ -114,3 +120,43 @@ layout = html.Div(
         "padding": "2rem",
     },
 )
+
+@app.callback(
+    [
+        Output('url_login', 'pathname'),
+        Output('login_alert', 'is_open'),
+        Output('login_alert', 'children')
+    ],
+    [
+        Input('login-button', 'n_clicks')
+    ],
+    [
+        State('login_username', 'value'),
+        State('login_password', 'value')
+    ]
+)
+def login_process(n_clicks, username, password):
+    if n_clicks > 0:
+        if username is None or password is None or username == "" or password == "":
+            return [dash.no_update, True, "Please enter both username and password."]
+        
+        sql = """
+            SELECT staff_id, staff_name, staff_username, staff_password, staff_role
+            FROM staff
+            WHERE staff_username = %s AND staff_delete_ind = FALSE
+        """
+
+        df = getDataFromDB(sql, (username,), ["staff_id", "staff_name", "staff_username", "staff_password", "staff_role"])
+        
+        if not df.empty:
+            stored_password = df.iloc[0]['staff_password']
+            
+            if stored_password == password:
+                # For now, we redirect to ownerdashboard as per the original href
+                return ["/ownerdashboard", False, ""]
+            else:
+                return [dash.no_update, True, "Incorrect password."]
+        else:
+            return [dash.no_update, True, "User not found."]
+            
+    raise PreventUpdate
